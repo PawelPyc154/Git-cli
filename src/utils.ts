a83143a11941f1cdd7d9c1bf6ca9a206552e3ea3
Paweł <pawelpyc154@gmail.comnpm>
2022-04-10 17:25:14 +0200

    add configuration

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,17 @@ import * as vscode from 'vscode'
 
 export const getBranch = (repository: Repository) => (repository.state.HEAD && repository.state.HEAD.name) || ''
 
+interface ConfigurationItem {
+  label: string
+  details: string
+}
+interface Configuration {
+  scopeOptions: ConfigurationItem[]
+  branchTypeOptions: ConfigurationItem[]
+  commitActionOptions: ConfigurationItem[]
+}
+const getConfiguration = () => vscode.workspace.getConfiguration('git-cli') as unknown as Configuration
+
 export const getGitExtension = () => {
   const vscodeGit = vscode.extensions.getExtension<GitExtension>('vscode.git')
   const gitExtension = vscodeGit && vscodeGit.exports
@@ -10,20 +21,20 @@ export const getGitExtension = () => {
 }
 
 export const getRepositorie = async () => {
-  const git123 = getGitExtension()
-  if (!git123) {
+  const gitExtension = getGitExtension()
+  if (!gitExtension) {
     await vscode.window.showErrorMessage('Unable to load Git Extension')
     return
   }
 
-  if (!git123.repositories.length) {
+  if (!gitExtension.repositories.length) {
     await vscode.window.showErrorMessage('No repositories')
     return
   }
-  if (git123.repositories.length > 1) {
+  if (gitExtension.repositories.length > 1) {
     const repositorie = await vscode.window
       .showQuickPick(
-        git123.repositories.map((repositorie) => ({
+        gitExtension.repositories.map((repositorie) => ({
           label: repositorie.rootUri.path,
           repositorie: repositorie,
         })),
@@ -35,25 +46,29 @@ export const getRepositorie = async () => {
       .then((item) => (item ? item.repositorie : undefined))
     return repositorie
   } else {
-    return git123.repositories[0]
+    return gitExtension.repositories[0]
   }
 }
 
-export const getScope = async () =>
-  await vscode.window
-    .showQuickPick([{ label: 'local-app' }, { label: 'common' }, { label: 'components' }], {
+export const getScope = async () => {
+  const { scopeOptions } = getConfiguration()
+  return await vscode.window
+    .showQuickPick(scopeOptions, {
       placeHolder: 'Please select a value of package-name',
       ignoreFocusOut: true,
     })
     .then((item) => (item ? item.label : undefined))
+}
 
-export const getCommitAction = async () =>
-  await vscode.window
-    .showQuickPick([{ label: 'added' }, { label: 'fixed' }, { label: 'removed' }], {
+export const getCommitAction = async () => {
+  const { commitActionOptions } = getConfiguration()
+  return await vscode.window
+    .showQuickPick(commitActionOptions, {
       placeHolder: 'Please select a value of commin action',
       ignoreFocusOut: true,
     })
     .then((item) => (item ? item.label : undefined))
+}
 
 export const getDescription = async () =>
   await vscode.window.showInputBox({
@@ -74,24 +89,15 @@ export const getTaskId = async (options?: { defaultTaskId?: string }) =>
     },
   })
 
-export const getBranchType = async () =>
-  await vscode.window
-    .showQuickPick(
-      [
-        { label: 'feature' },
-        { label: 'bugfix' },
-        { label: 'hotfix' },
-        {
-          label: 'internal',
-          details: 'interan change in monorepo, script, library updates etc.',
-        },
-      ],
-      {
-        placeHolder: 'Please select a value of branch-type',
-        ignoreFocusOut: true,
-      },
-    )
+export const getBranchType = async () => {
+  const { commitActionOptions } = getConfiguration()
+  return await vscode.window
+    .showQuickPick(commitActionOptions, {
+      placeHolder: 'Please select a value of branch-type',
+      ignoreFocusOut: true,
+    })
     .then((item) => (item ? item.label : undefined))
+}
 
 export const getConfirmedBranchName = async (options: { branchName: string }) =>
   await vscode.window.showInputBox({
